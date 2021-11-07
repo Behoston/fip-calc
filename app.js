68031c28368906ce6aa870bbd1e69dddecd8ac0b
Behoston <mlegiecki@gmail.com>
2021-11-08 00:12:13 +0100

    Save data in localStorage.

--- app.js
+++ app.js
@@ -10,6 +10,52 @@ const app = Vue.createApp({
             pojemnosc_fiolka: 5,
         }
     },
+    mounted() {
+        if (localStorage.waga_kg) {
+            this.waga_kg = localStorage.waga_kg
+        }
+        if (localStorage.dawka_mg_na_kg) {
+            this.dawka_mg_na_kg = localStorage.dawka_mg_na_kg
+        }
+        if (localStorage.stezenie_mg_na_ml) {
+            this.stezenie_mg_na_ml = localStorage.stezenie_mg_na_ml
+        }
+        if (localStorage.cena_wysylka) {
+            this.cena_wysylka = parseFloat(localStorage.cena_wysylka)
+        }
+        if (localStorage.dni_terapi) {
+            this.dni_terapi = localStorage.dni_terapi
+        }
+        if (localStorage.cena_fiolka) {
+            this.cena_fiolka = localStorage.cena_fiolka
+        }
+        if (localStorage.pojemnosc_fiolka) {
+            this.pojemnosc_fiolka = localStorage.pojemnosc_fiolka
+        }
+    },
+    watch: {
+        waga_kg(newwaga_kg) {
+            localStorage.waga_kg = newwaga_kg
+        },
+        dawka_mg_na_kg(newdawka_mg_na_kg) {
+            localStorage.dawka_mg_na_kg = newdawka_mg_na_kg
+        },
+        stezenie_mg_na_ml(newstezenie_mg_na_ml) {
+            localStorage.stezenie_mg_na_ml = newstezenie_mg_na_ml
+        },
+        cena_wysylka(newcena_wysylka) {
+            localStorage.cena_wysylka = newcena_wysylka
+        },
+        dni_terapi(newdni_terapi) {
+            localStorage.dni_terapi = newdni_terapi
+        },
+        cena_fiolka(newcena_fiolka) {
+            localStorage.cena_fiolka = newcena_fiolka
+        },
+        pojemnosc_fiolka(newpojemnosc_fiolka) {
+            localStorage.pojemnosc_fiolka = newpojemnosc_fiolka
+        }
+    },
     computed: {
         ilosc() {
             return (this.waga_kg * this.dawka_mg_na_kg) / this.stezenie_mg_na_ml;
